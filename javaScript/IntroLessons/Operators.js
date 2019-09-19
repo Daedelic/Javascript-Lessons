c95b273b49247f1b8aa4a5e2e6596175d5cc4fcd
Noah Mongeon <54866044+Daedelic@users.noreply.github.com>
2019-09-19 13:35:03 -0600

    Update Operators.js

--- javaScript/IntroLessons/Operators.js
+++ javaScript/IntroLessons/Operators.js
@@ -1,8 +1,16 @@
 
 let number = [1 39 46 59 68 100 5 102 '1']
+function simpleOperators (numberA, numberB);
+
 
 //simpleOperators
-let adding = number[0] number[1];
-//let subtraction
-//let division
-//console.log('1 + 39', adding);
+let adding = (numberA, numberB);
+let subtraction = (numberA, numberB);
+let division = (numberA, numberB);
+let multiplication = (numberA, numberB);
+let Modulus = (numberA, numberB);
+console.log(numberA, numberB), 'adding');
+console.log(numberA, numberB), 'subtraction');
+console.log(numberA, numberB), 'division');
+console.log(numberA, numberB), 'multiplication');
+console.log(numberA, numberB), 'modulus');
