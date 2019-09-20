c1241caec067f163e8ecbf5b436db3c303c30f13
Noah Mongeon <54866044+Daedelic@users.noreply.github.com>
2019-09-20 13:00:33 -0600

    Update Operators.js

--- javaScript/IntroLessons/Operators.js
+++ javaScript/IntroLessons/Operators.js
@@ -1,16 +1,28 @@
+//console.log("Did you change my file name?");
+console.log("You made it to the console!");
 
-let number = [1 39 46 59 68 100 5 102 '1']
-function simpleOperators (numberA, numberB);
+function simpleOperators (numberA, numberB) {
+  let adding = numberA + numberB;
+  let subtraction = numberA - numberB;
+  let mulitplying = numberA * numberB;
+  let division = numberA / numberB;
+  let modulus = numberA % numberB;
+  console.log(numberA, numberB, 'adding', adding);
+  console.log(numberA, numberB, 'subtraction', subtraction);
+  console.log(numberA, numberB, 'mulitplying', mulitplying);
+  console.log(numberA, numberB, 'division', division);
+  console.log(numberA, numberB, 'modulus', modulus);
+}
 
+skip count by _____ for _____ times starting at _____
+function skipCpinting (interval, times, startingNumber) {
+for ( let i = 1; i <= times; i ++ ) {
+    startingNumber = startingNumber + interval
+    console.log(startingNumber);
+}
 
-//simpleOperators
-let adding = (numberA, numberB);
-let subtraction = (numberA, numberB);
-let division = (numberA, numberB);
-let multiplication = (numberA, numberB);
-let Modulus = (numberA, numberB);
-console.log(numberA, numberB), 'adding');
-console.log(numberA, numberB), 'subtraction');
-console.log(numberA, numberB), 'division');
-console.log(numberA, numberB), 'multiplication');
-console.log(numberA, numberB), 'modulus');
+let number = [4, 7, 13, 99, 0, 100, 5, 1, '1'];
+
+simpleOperators(number[0], number[1]);
+simpleOperators(number[2], number[3]);
+simpleOperators(number[4], number[5]);
