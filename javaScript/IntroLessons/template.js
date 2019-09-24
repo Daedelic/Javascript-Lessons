5bc99b584a5eafcb69569dd40b3165049b36f28a
Noah Mongeon <54866044+Daedelic@users.noreply.github.com>
2019-09-24 13:51:37 -0600

    JHIH

---
+++ javaScript/IntroLessons/template.js
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+
+<html lang="en" dir="ltr">
+
+  <head>
+
+    <meta charset="utf-8">
+
+    <title>Project Title</title>
+
+    <meta name="author" content="Mark Mercer">
+    <meta name="description" content="xxx"> <!-- Webpage Description to poplulate search engines like Google-->
+    <meta name="keywords" content="xxx"> <!-- Key words for searching -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  </head>
+
+<body>
+
+    <header><h1>JavaScript Project</h1></header>
+    <nav><h2>Navigation Menu</h2></nav>
+    <aside><p>The Aside Space</p></aside>
+
+
+    <p>The list of assignments</p>
+        <ol>
+          <li> <a href="childPages/Landscape or portrait.html">Landscape or portrait</a>
+          <li> <a href="childPages/Max of Two Numbers">Max of Two Numbers</a>
+          <li> <a href="childPages/Modulus">Modulus Assignemnt</a>
+          <li> <a href="">Ordering Grades</a>
+          <li> <a href="">Demerit Points</a>
+          <li> <a href="">FizzBuzz</a>
+          <li> <a href="">Greeting Someone</a>
+        </ol>
+
+    <section>
+      <h3>bruh</h3>
+      <article>
+        <p>This is article 1 in section 1.</p>
+      </article>
+      <article>
+        <p>This is article 2 in section 1.</p>
+      </article>
+    </section>
+    <section>
+      <h3>Section 2</h3>
+      <article>
+        <p>This is article 1 in section 2.</p>
+      </article>
+      <article>
+        <p>This is article 2 in section 2.</p>
+      </article>
+    </section>
+
+  <script src="index.js"></script>
+
+  </body>
+
+  <footer>
+
+    <!--  Date Accessed: 20180920 -->
+    <p><strong>"i like ham."</strong>
+      <em><a href="" target="_blank">- Noah Mongeon</a></em></p>
+    <p>&copy Noah Mongeon on github</p>
+    <!-- Note: symbols in text get confused with HTML symbols, text-based symbols exist -->
+  </footer>
