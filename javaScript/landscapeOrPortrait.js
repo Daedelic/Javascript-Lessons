9b33ea84a39d87b14d419b3f31ce329f98b71179
Noah Mongeon <54866044+Daedelic@users.noreply.github.com>
2019-09-26 13:28:05 -0600

    Update landscapeOrPortrait.js

--- javaScript/landscapeOrPortrait.js
+++ javaScript/landscapeOrPortrait.js
@@ -1,8 +1,22 @@
+
+let stop = false;
+function testNaN (number) {
+  console.log(stop);
+  if (isNaN (number) ){
+      stop = true;
+      return "type a real number";
+    } else {
+
+
+
+
 function main () {
 let firstNumber, secondNumber, text;
 firstNumber = document.getElementByID("textfield1").value;
-document.getElementByID("validityTest1")testNaN(firstNumber);
+document.getElementByID("validityTest1").innerHTML = alert (testNaN (firstNumber));
+document.getElementByID("validityTest1").innerHTML = testNaN(firstNumber);
 
 secondNumber = document.getElementByID("textfield2").value;
-
+document.getElementByID("validityTest1").innerHTML = alert (testNaN (secondNumber));
+document.getElementByID("validityTest1").innerHTML = testNaN(secondNumber);
 }
