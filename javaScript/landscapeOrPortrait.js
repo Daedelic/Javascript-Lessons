c1aa73c4c700858c2225f4865e87c4031244c08c
Noah Mongeon <54866044+Daedelic@users.noreply.github.com>
2019-09-26 13:17:48 -0600

    sscscscs

---
+++ javaScript/landscapeOrPortrait.js
@@ -0,0 +1,8 @@
+function main () {
+let firstNumber, secondNumber, text;
+firstNumber = document.getElementByID("textfield1").value;
+document.getElementByID("validityTest1")testNaN(firstNumber);
+
+secondNumber = document.getElementByID("textfield2").value;
+
+}
